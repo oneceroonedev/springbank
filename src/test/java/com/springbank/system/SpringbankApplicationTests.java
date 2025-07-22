4cac270029e7d98beb7705957505ce5a57172431
Sonia <oneceroonedev@gmail.com>
2025-07-22 02:16:21 +0200

    Add Spring Boot base

---
+++ src/test/java/com/springbank/system/SpringbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbank.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
